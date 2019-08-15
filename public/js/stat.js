484fb23c773eae5cf0d6c1d364bbe8087a9ff97e
Bendeguz <symunona@gmail.com>
2019-08-15 11:12:51 +0200

    Fixed stats. Added better examles. Fixed title.

--- public/js/stat.js
+++ public/js/stat.js
@@ -1,82 +1,89 @@
 
-$(function(){
-	
-	var datamonthly2 = [] 
-	datamonthly.map(function(e){
-		var mdata = $.grep(datamonthly2,function(dy){
-			return dy.YM == e.YM
-		});
-		if (mdata.length){
-			mdata[0].cnt+=e.cnt;
+$(function () {
+
+	var datamonthly = [];
+
+	var db = window.db = JSON.parse($('meta[name="db"]').attr('content'))
+
+	db.blog.map(function (entry) {
+		var date = entry.date;
+		var Y = date.substr(0, 4)
+		var YM = Y + date.substr(5, 2)
+		datamonthly[YM] = datamonthly[YM] || { YM: YM, cnt: 0, len: 0, top: 0, Y: Y }
+		datamonthly[YM].cnt += 1
+		datamonthly[YM].len += entry.body.length
+		if (entry.top) {
+			datamonthly[YM].top++
 		}
-		else{
-			datamonthly2.push({
-				YM: e.YM,
-				cnt: e.cnt
-			})
-		}
+	})
+
+	var datamonthly2 = []
+	Object.keys(datamonthly).map(function (key) {
+		datamonthly2.push({
+			YM: datamonthly[key].YM,
+			cnt: datamonthly[key].cnt,
+			len: datamonthly[key].len
+		})
 	});
-	
-	var publicMonthly = datamonthly.filter(function(e){return e.top})
-	var privateMonthly = datamonthly.filter(function(e){return !e.top})
+
+	var publicMonthly = datamonthly.filter(function (e) { return e.top })
+	var privateMonthly = datamonthly.filter(function (e) { return !e.top })
 
 	var statsmonthly = {
-	    labels: publicMonthly.map(function(e){return e.YM}),
-	    datasets: [       
-	        {
-	            label: "Public",
-	            fillColor: "rgba(151,187,205,0.5)",
-	            strokeColor: "rgba(151,187,205,0.8)",
-	            highlightFill: "rgba(151,187,205,0.75)",
-	            highlightStroke: "rgba(151,187,205,1)",
-	            data: publicMonthly.map(function(e){return e.cnt})
-	        },
-	         {
-	            label: "Private",
-	            fillColor: "rgba(151,87,95,0.5)",
-	            strokeColor: "rgba(151,87,95,0.8)",
-	            highlightFill: "rgba(151,87,95,0.75)",
-	            highlightStroke: "rgba(151,87,95,1)",
-	            data: privateMonthly.map(function(e){return e.cnt})
-	        },	        
-	         {
-	            label: "All",
-	            fillColor: "rgba(151,187,95,0.5)",
-	            strokeColor: "rgba(151,187,95,0.8)",
-	            highlightFill: "rgba(151,187,95,0.75)",
-	            highlightStroke: "rgba(151,187,95,1)",
-	            data: datamonthly2.map(function(e){return e.cnt})
-	        }
-	        
-	    ]
+		labels: publicMonthly.map(function (e) { return e.YM }),
+		datasets: [
+			{
+				label: "Public",
+				fillColor: "rgba(151,187,205,0.5)",
+				strokeColor: "rgba(151,187,205,0.8)",
+				highlightFill: "rgba(151,187,205,0.75)",
+				highlightStroke: "rgba(151,187,205,1)",
+				data: publicMonthly.map(function (e) { return e.cnt })
+			},
+			{
+				label: "Private",
+				fillColor: "rgba(151,87,95,0.5)",
+				strokeColor: "rgba(151,87,95,0.8)",
+				highlightFill: "rgba(151,87,95,0.75)",
+				highlightStroke: "rgba(151,87,95,1)",
+				data: privateMonthly.map(function (e) { return e.cnt })
+			},
+			{
+				label: "All",
+				fillColor: "rgba(151,187,95,0.5)",
+				strokeColor: "rgba(151,187,95,0.8)",
+				highlightFill: "rgba(151,187,95,0.75)",
+				highlightStroke: "rgba(151,187,95,1)",
+				data: datamonthly2.map(function (e) { return e.cnt })
+			}
+
+		]
 	};
 	var datayearly = []
-	datamonthly.map(function(e){
-		var yeardata = $.grep(datayearly,function(dy){
-			return dy.y == e.Y
-		});
-		if (yeardata.length){
-			yeardata[0].cnt+=e.cnt;
+	Object.keys(datamonthly).map(function (key) {
+		var yeardata = datayearly.find((yearData) => yearData.y === key.substr(0, 4))
+		if (yeardata) {
+			yeardata.cnt += datamonthly[key].cnt;
 		}
-		else{
+		else {
 			datayearly.push({
-				y: e.Y,
-				cnt: e.cnt
+				y: datamonthly[key].Y,
+				cnt: datamonthly[key].cnt
 			})
 		}
 	})
 	var statsyearly = {
-	    labels: datayearly.map(function(e){return e.y}),
-	    datasets: [       
-	        {
-	            label: "Years",
-	            fillColor: "rgba(151,187,205,0.5)",
-	            strokeColor: "rgba(151,187,205,0.8)",
-	            highlightFill: "rgba(151,187,205,0.75)",
-	            highlightStroke: "rgba(151,187,205,1)",
-	            data: datayearly.map(function(e){return e.cnt})
-	        }
-	    ]
+		labels: datayearly.map(function (e) { return e.y }),
+		datasets: [
+			{
+				label: "Years",
+				fillColor: "rgba(151,187,205,0.5)",
+				strokeColor: "rgba(151,187,205,0.8)",
+				highlightFill: "rgba(151,187,205,0.75)",
+				highlightStroke: "rgba(151,187,205,1)",
+				data: datayearly.map(function (e) { return e.cnt })
+			}
+		]
 	};
 
 	var ctx = document.getElementById("graph").getContext("2d");
--- routes/admin.js
+++ routes/admin.js
@@ -7,29 +7,26 @@ var path = require('path');
 var db = require('../lib/db');
 
 exports.stat = function(req, res){
-	
-  var query = "select year(date) as Y, month(date) as M, concat(year(date) ,'-', month(date)) as YM, count(id) as cnt, top from blog group by year(date),month(date), top;"
-  var ret = api.processentries({});
-  
-  db.query(query).done(function(data){
-  	ret.statdata = data[0];
-  	res.render('stat',  ret);
-  })
-  
-  
-}; 
+  	res.render('stat', {db: db.db});
+};
+
+exports.dump = function(req, res){
+	res.setHeader('content-type', 'application/json')
+	res.send(JSON.stringify(db.db))
+	res.end()
+};
 
 
 exports.content = function(req, res){
-	
+
  var dir = module.parent.filename.replace(/\\/g,'/');
  dir = dir.slice(0,dir.lastIndexOf("/")) +"/public/content";
-	
+
 	fs.readdir(dir, function (err, files) {
 		if (!files) {
 			res.send(err);
 			return 0;
-		}		
+		}
 	    res.send(files.map(function (file) {
 	        return {file: file, path: path.join(dir, file)};
 	    }).filter(function (file) {
@@ -45,26 +42,26 @@ exports.content = function(req, res){
 	    	if (a.ctime < b.ctime) return 1;
 	    	return 0;
 	    }));
-	    
+
 	});
-	
+
 };
 
 exports.toggleTop = function(req, res) {
 	let entry = db.getById('blog', parseInt(req.params.id))
 	entry.top = !entry.top
-	db.persist()	
+	db.persist()
 	res.send(entry)
 };
 
-exports.saveEntry = function (req, res) {	
+exports.saveEntry = function (req, res) {
 	res.send(db.saveOrUpdate('blog', req.body))
 }
 
-exports.deleteTag = function (req, res) {	
+exports.deleteTag = function (req, res) {
 	res.send(db.deleteItem('tags', req.params.id))
 }
 
-exports.saveTag = function (req, res) {	
+exports.saveTag = function (req, res) {
 	res.send(db.saveOrUpdate('tags', req.body))
 }
